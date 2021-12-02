bdf226cb504f91b109abbc54f99a718b2c94199b
Drake Woodsky <drdrake1337@gmail.com>
2021-12-03 01:16:12 +0200

    Product Dish

---
+++ CalculatorOfCalories/CalculatorOfCalories/Dish.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorOfCalories
+{
+    namespace Logic
+    {
+        class Dish
+        {
+            private List<Product> products;
+
+            public Dish()
+            {
+                products = new List<Product>();
+            }
+
+            public Dish(List<Product> _products)
+            {
+                products = _products;
+            }
+
+            public Product this[int index]
+            {
+                get
+                {
+                    if (index >= 0 && index < products.Count)
+                    {
+                        return products[index];
+                    }
+                    else
+                    {
+                        throw new Exception("Некорректный индекс! " + index);
+                    }
+                }
+                set
+                {
+                    if (index >= 0 && index < products.Count)
+                    {
+                        products[index] = value;
+                    }
+                    else
+                    {
+                        throw new Exception("Некорректный индекс! " + (index - min));
+                    }
+                }
+            }
+
+            public void Add(Product product)
+            {
+                products.Add(product);
+            }
+
+            public void Delete(Product product)
+            {
+                products.Remove(product);
+            }
+
+            public void DeleteByIndex(int index)
+            {
+                products.RemoveAt(index);
+            }
+
+            public int GetIndex(Product product)
+            {
+                return products.IndexOf(product);
+            }
+        }
+    }
+
+    
+}
---
+++ CalculatorOfCalories/CalculatorOfCalories/Product.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorOfCalories
+{
+    namespace Logic
+    {
+
+        class Product
+        {
+            private string name;
+            private double calories_per_100_gramms;
+            private double mass;
+
+            public string Name
+            {
+                get { return name; }
+                set 
+                {
+                    if (value.Trim().Length == 0)
+                     throw new Exception("имя продукта не может быть пустым или состоять из одних пробелов");
+
+                    name = value;
+                }
+            }
+
+            public double CaloriesPer100Gramms
+            {
+                get { return calories_per_100_gramms; }
+                set
+                {
+                    if (value < 0)
+                        throw new Exception("нельзя есть объекты с отрицательной энергией!");
+
+                    calories_per_100_gramms = value;
+                }
+            }
+
+            public double MassInKilo
+            {
+                get { return mass; }
+                set
+                {
+                    if (value < 0)
+                        throw new Exception("нельзя есть объекты с отрицательной массой!");
+
+                    mass = value;
+                }
+            }
+
+            public double GetTotalCalories()
+            {
+                return mass * calories_per_100_gramms * 10;
+            }
+        }
+    }
+}
